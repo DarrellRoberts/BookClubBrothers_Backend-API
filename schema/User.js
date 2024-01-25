a57b6fc8a7e2e436fa70488be432af9f01d7f956
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-01-25 22:29:15 +0100

    change structure schema

--- schema/User.js
+++ schema/User.js
@@ -27,7 +27,7 @@ const userSchema = new Schema ({
             type: String,
         }
     },
-    favGenre:
+    favGenre: [
         {
             type: [String],
             enum: [
@@ -47,6 +47,7 @@ const userSchema = new Schema ({
                 "Non-fiction"
             ],
         },
+    ],
     profileURL: {
         type: String,
     },
