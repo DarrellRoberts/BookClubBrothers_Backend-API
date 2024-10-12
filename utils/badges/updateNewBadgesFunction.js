d2370901cb23da6e5167fff488c66ef580cfb120
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-10-12 23:58:47 +0200

    0007: added badges to User schema

---
+++ utils/badges/updateNewBadgesFunction.js
@@ -0,0 +1,24 @@
+// only use this function to add new badges
+
+const User = require("../../schema/User");
+
+async function updateNewBadgesFunction() {
+    try {
+        const users = await User.find();
+
+        for (const user of users) {
+            user.userInfo.badges = {
+                allbooks: false,
+                loneWolf: false,
+                mostBooks: false,
+            };
+            await user.save();
+        }
+
+        console.log('All user profiles updated with badges.');
+    } catch (error) {
+        console.error('Error updating user profiles:', error);
+    }
+}
+
+// updateNewBadgesFunction();
