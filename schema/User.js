0c015addadbc8956e584a4cea4cd458893fcc82d
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-10-25 11:07:02 +0200

    0012: added lastLoggedIn property

--- schema/User.js
+++ schema/User.js
@@ -19,6 +19,10 @@ const userSchema = new Schema ({
         type: Boolean,
         default: false
     },
+    lastLoggedIn: {
+        type: String,
+        default: "",
+    },
     userInfo: {
         dob: {
             type: Date
